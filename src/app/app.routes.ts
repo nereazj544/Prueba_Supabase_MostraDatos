a1e364c9778301a2f1634b8f201be397e93e44fa
nereazj544 <nzjvirtual@gmail.com>
2025-05-14 12:57:40 +0200

    feat: Agregar componente de inicio de sesión con validación y redirección según rol

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ProveedorComponent } from './perfil/proveedor/proveedor.component';
+import { AuthLogInComponent } from './src/app/auth/auth-login/auth-login.component';
 
 export const routes: Routes = [
     { path: 'perfil/proveedor', component: ProveedorComponent },
+    { path: 'login', component: AuthLogInComponent },
 ];
